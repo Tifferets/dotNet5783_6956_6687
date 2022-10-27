aa2e1c26f7c6647b88d9b702a08e23948bf73aa6
Tifferet Sonnenberg <sonnenbe@g.jct.ac.il>
2022-10-27 18:17:02 +0300

    welcom function

---
+++ dotNet5783_6956_6687/stage0/Program6956.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stage0
+{
+    partial class Program
+    {
+       static partial void Welcome6956()
+            {
+                Console.WriteLine("i am also here!");
+            }
+        }
+    }
+}
