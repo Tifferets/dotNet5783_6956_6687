73736c2d7588e133c2e05019c95862b031a6b9ce
Tifferet Sonnenberg <sonnenbe@g.jct.ac.il>
2022-11-06 23:53:12 +0200

    add 3

--- dotNet5783_6956_6687/DalTest/Program.cs
+++ dotNet5783_6956_6687/DalTest/Program.cs
@@ -1,37 +1,54 @@
 ﻿using Dal;
+using DO;
+using DalList;
 
-using System;
-namespace DalTest
+namespace DalTest;
+
+partial class program
 {
-    partial class program
+    private DalOrder dalOrder = new DalOrder();
+    private DalProduct dalProduct = new DalProduct();
+    private DalOrderItem dalOrderItem = new DalOrderItem();
+    static void Main(string[] args)
     {
-        private DalOrder dalOrder = new DalOrder();
-        private DalProduct dalProduct = new DalProduct();
-        private DalOrderItem dalOrderItem = new DalOrderItem();
-        static void Main(string[] args)
-        {
 
-            Console.WriteLine(@" 
+        Console.WriteLine(@" 
 0: exit
 1: Order
-2: Product
-3: Order Item
+2: Order Item 
+3:Product
              ");
 
-            int choice1 = Console.Read();
-            while (choice1 !=0)
-        {
-            Console.WriteLine(@" 
+        int choice1 = Console.Read();
+        while (choice1 !=0)
+    {
+        Console.WriteLine(@" 
             1.Add
             2.Show Using ID
             3.Show List
             4.Update 
             5.Delete ");
-            int choice2 = Console.Read();
-            if(choice1== 1)
+        int choice2 = Console.Read();
+        if(choice1== 1)
+             {
+                switch (choice1)
                  {
-                    switch (choice1)
-                     {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                 } 
+              }
+            if (choice1 == 2)
+            { 
+                switch (choice1)
+                {
                     case 1:
                         break;
                     case 2:
@@ -42,45 +59,42 @@ namespace DalTest
                         break;
                     case 5:
                         break;
-                     } 
-                  }
-                if (choice1 == 2)
-                { 
-                    switch (choice1)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                        case 5:
-                            break;
 
-                    } 
-                }
-                if (choice1 == 3)
+                } 
+            }
+            if (choice1 == 3)// pro
+            {
+                switch (choice1)
                 {
-                    switch (choice1)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                        case 5:
-                            break;
-                    }
+                    case 1://add
+                        Product product = new Product();
+                        Console.WriteLine("Enter product name");
+                        product.Name = Console.ReadLine();
+                        Console.WriteLine("Enter product Category");
+                        product.Category= Console.ReadLine();
+                        Console.WriteLine("Enter product Price");
+                        product.Price = Console.Read();
+                        Console.WriteLine("Enter product amount");
+                        product.InStock = Console.Read();
+                        
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter ID");
+                        int id = Console.Read();
+                        
+
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
                 }
-        }
+            }
     }
 }
-
+}
 
 
 
