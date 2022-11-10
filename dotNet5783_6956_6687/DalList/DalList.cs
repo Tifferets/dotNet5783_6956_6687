2066c69514ca711244eeea683b3618b584bc08b7
rachelliAA <rachelli@g.jct.ac.il>
2022-11-10 13:20:06 +0200

    dal list properties

--- dotNet5783_6956_6687/DalList/DalList.cs
+++ dotNet5783_6956_6687/DalList/DalList.cs
@@ -7,7 +7,9 @@ using DalApi;
 namespace Dal
 {
     sealed public class DalList:  IDal
-    { 
-
+    {
+        public IOrder order => new DalOrder();
+        public IOrderItem orderItem => new DalOrderItem();
+        public IProduct product => new DalProduct();
     }
 }
