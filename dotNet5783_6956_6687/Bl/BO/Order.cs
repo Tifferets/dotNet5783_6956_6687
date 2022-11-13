e86a61f8300484aaccf80450f7e0b183e9aacc59
rachelliAA <rachelli@g.jct.ac.il>
2022-11-13 19:05:46 +0200

    order in BO

--- dotNet5783_6956_6687/Bl/BO/Order.cs
+++ dotNet5783_6956_6687/Bl/BO/Order.cs
@@ -4,4 +4,12 @@ namespace BO;
 
 public class Order
 {
+    public int Id { get; set; } 
+    public string CustomerName { get; set; }
+    public string CustomerEmail { get; set; }
+    public string CustomerAddress { get; set; } 
+    public DateTime OrderDate { get; set; } 
+    public DateTime OrderShip { get; set; } 
+    public DateTime OrderDelivery { get; set; }
+
 }
