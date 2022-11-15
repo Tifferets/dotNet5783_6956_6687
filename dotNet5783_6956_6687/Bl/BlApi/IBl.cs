467efa756f1780b5d86559a1517338d0d9ae2fa8
Tifferet Sonnenberg <sonnenbe@g.jct.ac.il>
2022-11-15 13:38:07 +0200

    addad IBl

---
+++ dotNet5783_6956_6687/Bl/BlApi/IBl.cs
@@ -0,0 +1,11 @@
+﻿
+using DalApi;
+
+namespace BlApi;
+
+public interface IBl
+{
+    public IOrder Order { get; }
+    public IProduct Product { get; }
+    public ICart Cart { get; }
+}
--- dotNet5783_6956_6687/Bl/BlApi/Product.cs
+++ dotNet5783_6956_6687/Bl/BlApi/Product.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace BlApi
 {
-    internal class Product
+    public class Product
     {
+
     }
 }
