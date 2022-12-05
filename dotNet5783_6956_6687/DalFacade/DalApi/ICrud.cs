26e25f9cd23f5e0aa7e8abe59c71037da111e0be
Tifferet Sonnenberg <sonnenbe@g.jct.ac.il>
2022-12-05 18:04:29 +0200

    nullables

--- dotNet5783_6956_6687/DalFacade/DalApi/ICrud.cs
+++ dotNet5783_6956_6687/DalFacade/DalApi/ICrud.cs
@@ -11,5 +11,5 @@ public interface ICrud<T> /*where T: struct*/
     void Delete(int id);
     int Add(T other);
     IEnumerable<T?> GetAll(Func<T?, bool>? select = null);
-    T? GetSingle(Func<T?, bool>? func);
+    T GetSingle(Func<T?, bool>? func);
 }
