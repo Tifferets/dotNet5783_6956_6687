35f648e23898f2c83b8f140289f3daee99f2f221
rachelliAA <rachelli@g.jct.ac.il>
2023-01-09 11:41:10 +0200

    xml tool again

--- dotNet5783_6956_6687/DalXml/XMLTools.cs
+++ dotNet5783_6956_6687/DalXml/XMLTools.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Dal
 {
-    internal class XMLTools
+    class XMLTools
     {
+        static string dir = @"xml\";
+        static XMLTools()
+        {
+            if(!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+        }
+        //  static string fpath = @"Order";
+        public static void SaveListToXML<T>(List<T> listOfOrders, string path)
+        {
+            try
+            {
+
+                FileStream fs = new FileStream(dir + path, FileMode.Create);
+                XmlSerializer x = new XmlSerializer(listOfOrders.GetType());
+                x.Serialize(fs, listOfOrders);
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                     //  throw new DO.CantSaveListToXMLException();
+            } 
+        }
+        public static List<T> LoadListFromXML<T>(string path)
+        {
+            try
+            {
+                if (File.Exists(dir + path))
+                {
+                    List<T> list;
+                    XmlSerializer x = new XmlSerializer(typeof(List<T>));
+                    FileStream fs = new FileStream(dir + path, FileMode.Open);
+                    list = (List<T>)x.Deserialize(fs);
+                    fs.Close();
+                    if (list != null)
+                        return list;
+                    else
+                        throw new DalApi.EmptyListException();
+                }
+                else
+                    return new List<T>();
+            }
+            catch(Exception ex)
+            {
+                throw new DalApi.CantGetListFromXMLException();
+            }
+        }
     }
 }
