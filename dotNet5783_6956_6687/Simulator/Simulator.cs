fb27de17aa9a545ffed7c48e8e347a49ecb4cd37
rachelliAA <rachelli@g.jct.ac.il>
2023-01-16 18:20:03 +0200

    started simulater

---
+++ dotNet5783_6956_6687/Simulator/Simulator.cs
@@ -0,0 +1,11 @@
+﻿namespace Simulator
+{
+    public static class Simulator
+    {
+        private static volatile bool stop;
+        private static bool isRunning;//if false we want to to stop running
+        public static void StartSimulator() { }
+        public static void StopSimulator() { }
+        //public static 
+    }
+}
