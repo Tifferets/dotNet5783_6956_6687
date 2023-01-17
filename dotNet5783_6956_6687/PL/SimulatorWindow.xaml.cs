dc59c6097021beeb900e08d90fe20b4c5baa8bb3
Tifferet Sonnenberg <sonnenbe@g.jct.ac.il>
2023-01-17 12:59:50 +0200

    backgroud worker

--- dotNet5783_6956_6687/PL/SimulatorWindow.xaml.cs
+++ dotNet5783_6956_6687/PL/SimulatorWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.ComponentModel;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace PL
 {
@@ -19,9 +21,48 @@ namespace PL
     /// </summary>
     public partial class SimulatorWindow : Window
     {
+        BackgroundWorker simulator_bgw;
         public SimulatorWindow()
         {
             InitializeComponent();
+            simulator_bgw = new BackgroundWorker();
+            simulator_bgw.DoWork += Simulator_bgw_DoWork;
+            simulator_bgw.ProgressChanged += Simulator_bgw_ProgressChanged;
+            simulator_bgw.RunWorkerCompleted += Simulator_bgw_RunWorkerCompleted;
+
+            simulator_bgw.WorkerSupportsCancellation= true;
+            simulator_bgw.WorkerReportsProgress= true;
+        }
+
+        private void Simulator_bgw_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Simulator_bgw_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Simulator_bgw_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)//start the simulator button
+        {
+            if(simulator_bgw.IsBusy != true) //if its not in the middle of working
+            {
+                simulator_bgw.RunWorkerAsync();//we need to send the length of our loop- how long it will take 
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)//cancle the simulator button
+        {
+            if (simulator_bgw.WorkerSupportsCancellation == true)//if it supports canclation
+            {
+                simulator_bgw.CancelAsync();// Cancel the asynchronous operation- cancle the thread
+            }
         }
     }
 }
