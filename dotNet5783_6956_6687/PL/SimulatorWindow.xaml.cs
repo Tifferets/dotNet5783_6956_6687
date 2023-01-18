b00c1018fad184306c3b39ba7acd4cf76ef6a622
rachelliAA <rachelli@g.jct.ac.il>
2023-01-18 22:56:17 +0200

    i dont want these changes

--- dotNet5783_6956_6687/PL/SimulatorWindow.xaml.cs
+++ dotNet5783_6956_6687/PL/SimulatorWindow.xaml.cs
@@ -146,20 +146,7 @@ namespace PL
         }
         
     }
-    //
-    // C# Code, CustomWindow class declaration
-    //
-    public partial class CustomWindow : Window
-    {
-        // Prep stuff needed to remove close button on window
-        private const int GWL_STYLE = -16;
-        private const int WS_SYSMENU = 0x80000;
-        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
-        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-        }
-    }
+  
 }
 public enum OrderStatus
 {
@@ -171,19 +158,9 @@ public class OrderPl : DependencyObject
     public int ID { get; set; }
     //public Status PersonalStatus { get; set; }
 
-
-        public CustomWindow()
-        {
-            Loaded += ToolWindow_Loaded;
-        }
-
-        void ToolWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            // Code to remove close box from window
-            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
-            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
-        }
-    }
+}
+       
+    
 
     // Using a DependencyProperty as the backing store for PersonalStatus.  This enables animation, styling, binding, etc...
     public static readonly DependencyProperty StatusProperty =
